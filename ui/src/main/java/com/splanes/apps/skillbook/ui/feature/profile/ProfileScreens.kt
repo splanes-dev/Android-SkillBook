4c2650ec52b2f54353cf3a0fce47ed47c38bc89b
Sergi Planes <sergio.planes@adevinta.com>
2023-03-02 17:54:37 +0100

    feature(profile): profile screen scaffold

--- ui/src/main/java/com/splanes/apps/skillbook/ui/feature/profile/ProfileScreens.kt
+++ ui/src/main/java/com/splanes/apps/skillbook/ui/feature/profile/ProfileScreens.kt
@@ -1,4 +1,26 @@
 package com.splanes.apps.skillbook.ui.feature.profile
 
-class ProfileScreens {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.splanes.apps.skillbook.ui.R
+import com.splanes.apps.skillbook.ui.components.topbar.SkillBookTopBar
+
+@Composable
+fun ProfileScreen(
+    uiState: ProfileUiState.Profile
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            SkillBookTopBar(title = stringResource(id = R.string.profile))
+        }
+    ) { innerPaddingValues ->
+        Column(modifier = Modifier.padding(innerPaddingValues)) {
+        }
+    }
 }
